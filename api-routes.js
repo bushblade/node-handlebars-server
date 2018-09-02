8da4231fbc52c77d882ad5784fe47c71bd412f3d
Will Adams <bushblade@gmail.com>
2018-09-02 21:01:50 +0100

    install cors

--- api-routes.js
+++ api-routes.js
@@ -1,20 +1,21 @@
 const random = require('./ponyname')
+const cors = require('cors')
 
 const api = app => {
 
-  app.get('/api', (req, res) => {
+  app.get('/api', cors(), (req, res) => {
     res.status(200).send({
       message: 'You reached Bushblades API'
     })
   })
 
-  app.get('/api/random-pony-name', (req, res) => {
+  app.get('/api/random-pony-name', cors(),  (req, res) => {
     res.status(200).send({
       name: `${random()}`
     })
   })
 
-  app.get('/api/:test', (req, res) => {
+  app.get('/api/:test', cors(), (req, res) => {
     res.status(200).send({
       message: `you reached the ${req.params.test} route`
     })
