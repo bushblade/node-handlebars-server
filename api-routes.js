0c3ae62014abaf29a2b945000737e362cfa650a3
Will Adams <bushblade@gmail.com>
2018-09-02 20:53:40 +0100

    cors test

--- api-routes.js
+++ api-routes.js
@@ -2,6 +2,13 @@ const random = require('./ponyname')
 
 const api = app => {
 
+  // CORS
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next()
+  })
+
   app.get('/api', (req, res) => {
     res.status(200).send({
       message: 'You reached Bushblades API'
