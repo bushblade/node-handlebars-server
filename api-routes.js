ebca83da432e546154ee571dec2ac069b942bfce
Will Adams <bushblade@gmail.com>
2018-11-21 15:04:04 +0000

    forgot to export!

--- api-routes.js
+++ api-routes.js
@@ -24,10 +24,13 @@ const api = app => {
 
   app.get('/api/imgur/:album', cors(), (req, res) => {
     const { album } = req.params.album
-    axios.get(`https://api.imgur.com/3/album/${album}`, imgurOptions).then(r => {
-      console.log(r)
-      res.status(200).send(r)
-    })
+    axios
+      .get(`https://api.imgur.com/3/album/${album}`, imgurOptions)
+      .then(r => {
+        console.log(r)
+        res.status(200).send(r)
+      })
+      .catch(err => res.send(err))
   })
 }
 
