5d064926c752fd939a72d5102174f5752c3cf55d
Will Adams <bushblade@gmail.com>
2018-11-21 14:35:05 +0000

    added axios and imgur api route

--- api-routes.js
+++ api-routes.js
@@ -1,15 +1,16 @@
 const random = require('./ponyname')
 const cors = require('cors')
+const imgurOptions = require('./imgur-options')
+const axios = require('axios')
 
 const api = app => {
-
   app.get('/api', cors(), (req, res) => {
     res.status(200).send({
       message: 'You reached Bushblades API'
     })
   })
 
-  app.get('/api/random-pony-name', cors(),  (req, res) => {
+  app.get('/api/random-pony-name', cors(), (req, res) => {
     res.status(200).send({
       name: `${random()}`
     })
@@ -19,6 +20,13 @@ const api = app => {
     res.status(200).send({
       message: `you reached the ${req.params.test} route`
     })
+  })
+
+  app.get('/api/imgur/:album', cors(), (req, res) => {
+    const { album } = req.params.album
+    axios
+      .get(`https://api.imgur.com/3/album/${album}`, imgurOptions)
+      .then(r => res.status(200).send(r))
   })
 }
 
